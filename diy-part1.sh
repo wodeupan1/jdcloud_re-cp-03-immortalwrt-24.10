230e6fcbd2821fb191a5fc9aeeab84609b7ef7ee
Walter San <wodeupan1@163.com>
2025-07-17 09:29:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
